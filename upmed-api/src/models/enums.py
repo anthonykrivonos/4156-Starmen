260853d9e578b373f09c361618d8a2264e20b75b
Anthony Krivonos <abkrivonos@gmail.com>
2020-11-06 10:47:27 -0500

    Added 'NEVER' to Status enum

--- upmed-api/src/models/enums.py
+++ upmed-api/src/models/enums.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 class Status(Enum):
     ACTIVE = 0
-    PAST = 1
-    REMISSION = 2
-    CURED = 3
+    NEVER = 1
+    PAST = 2
+    REMISSION = 3
+    CURED = 4
--- upmed-web/src/models/enums.ts
+++ upmed-web/src/models/enums.ts
@@ -1,6 +1,7 @@
 export enum Status {
     ACTIVE = 0,
-    PAST = 1,
-    REMISSION = 2,
-    CURED = 3,
+    NEVER = 1,
+    PAST = 2,
+    REMISSION = 3,
+    CURED = 4,
 }
