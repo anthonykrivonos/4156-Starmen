498a6bb66a6dfcc0a4384f6b493c9fb350265aec
Anthony Krivonos <abkrivonos@gmail.com>
2020-12-01 20:21:19 -0500

    Urgent logs fix to show all logs

--- upmed-api/src/api/logs/logs.py
+++ upmed-api/src/api/logs/logs.py
@@ -3,22 +3,14 @@ from datetime import datetime
 
 from flask import Blueprint
 
-from sys import path
-from os.path import join, dirname
-path.append(join(dirname(__file__), '../../..'))
-
-from src.util.env import Env
+from .logs_helper import get_logs
 
 
 logs_endpoints = Blueprint('logs', __name__)
 
-heroku_conn = heroku3.from_key(Env.HEROKU_API_KEY())
-app = heroku_conn.apps()['upmed-api']
-
-
 @logs_endpoints.route('/', methods=['GET'])
 def root():
-    logs = app.get_log()
+    logs = get_logs()
     res = """
         <h1>Upmed Logs</h1>
         <h4>Last Updated %s</h4>
